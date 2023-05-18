31903ab6669362816d8dfcac6f8eb148a5691075
Andrei-Laurențiu Radu <117320481+andreilaurentiuradu@users.noreply.github.com>
2023-05-18 09:15:34 +0300

    Update tema3.c
    120/120

--- tema3.c
+++ tema3.c
@@ -14,106 +14,11 @@ void link(int a, int b, int father[]);
 int cmp_edge(const void *a, const void *b);
 int cmp(const void *a, const void *b);
 void add_location(char **name, int index, char *location);
+void swap(int *a, int *b);
+void selection_sort(int index[], int n, int dist[], int depths[]);
 
-void swap(int *a, int *b) {
-    int aux = *a;
-    *a = *b;
-    *b = aux;
-}
-
-void selection_sort(int index[], int n, int dist[], int depths[]) {
-    for (int i = 0; i < n; ++i) {
-        for (int j = i + 1; j < n; ++j) {
-            if (((float)dist[index[i]]) / depths[index[i]] > ((float)dist[index[j]]) / depths[index[j]])
-                // / depths[index[i]]
-                // / depths[index[j]]
-                swap(&index[i], &index[j]);
-        }
-    }
-}
-void dijkstra(FILE *out, char **name, graph *g, int depths[], int n, int start, int stop, int weight) {
-    int *in_queue = (int *)calloc(n, sizeof(int));
-    int *queue = (int *)calloc(n, sizeof(int));
-    int pos_queue = 0;
-    int *dist = (int *)malloc(n * sizeof(int));
-    int *father = (int *)malloc(n * sizeof(int));
-    int *son = (int *)malloc(n * sizeof(int));
-
-    for(int i = 0; i < n; ++i) {
-        father[i] = i;
-    }
-
-    for (int i = 0; i < n; ++i) {
-        dist[i] = INFINIT;
-    }
-    // punem nodul de start in coada si actualizam distanta
-    dist[start] = 0;
-    queue[pos_queue] = start;
-    in_queue[start] = 1;
-    int current;
-    node *neigh;
-    while (pos_queue != -1) {
-        // scoatem un nod din coada
-        current = queue[pos_queue];
-        pos_queue--;
-        in_queue[current] = 0;
-        neigh = g[current].adj_list;
-        for (int i = 0; i < g[current].adj_list->index; ++i) {
-            neigh = neigh->next;
-            float actual_score =
-                (float)(dist[neigh->index]) / depths[neigh->index];
-            float new_score = (float)(dist[current] +
-                                      neigh->cost) / depths[neigh->index];
-            if (actual_score > new_score) {
-                // actualizam distanta
-                dist[neigh->index] = dist[current] + neigh->cost;
-                father[neigh->index] = current;
-                // daca nu e in coada
-                if (in_queue[neigh->index] == 0) {
-                    in_queue[neigh->index] = 1;  // il adaugam
-                    pos_queue++;
-                    queue[pos_queue] = neigh->index;
-                    selection_sort(queue, pos_queue + 1, dist, depths);
-                }
-            }
-        }
-    }
-
-    //for (int i = 0; i < n; ++i) printf("name: %s %d\n", name[i], dist[i]);
-    //printf("\n");
-    int total_cost = dist[stop];
-
-
-    son[stop] = -1;
-    while(stop != start){
-        // printf("index %d\n", stop);
-        son[father[stop]] = stop;
-        stop = father[stop];
-    }
-
-    // printf("\n");
-    int depthest = INFINIT;
-
-    fprintf(out, "%s ", name[start]);
-    start = son[start];
-
-    while(son[start] != -1){
-        if(depthest > depths[start])
-            depthest = depths[start];
-        fprintf(out, "%s ", name[start]);
-        start = son[start];
-    }
-    fprintf(out, " %s\n", name[start]);
-    fprintf(out, "%d\n", total_cost);
-    fprintf(out, "%d\n", depthest);
-    fprintf(out, "%d", weight / depthest);
-
-    free(father);
-    free(son);
-    free(queue);
-    free(dist);
-    free(in_queue);
-}
+void dijkstra(FILE *out, char **name, graph *g, int depths[], int n, int start,
+              int stop, int weight);
 
 int main(int argc, char const *argv[]) {
     FILE *in = fopen("tema3.in", "r");
